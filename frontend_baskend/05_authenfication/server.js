e0b6f1a66aee0469fd5e46327532d68f8c5379f3
Michael Houmann <michael@michael-h.dk>
2016-11-05 11:49:02 +0100

    Added interface design

--- frontend_baskend/05_authenfication/server.js
+++ frontend_baskend/05_authenfication/server.js
@@ -46,6 +46,54 @@ app.get('/', function(req, res) {
 //gen an instance of the express router
 var apiRouter = express.Router();
 
+//middelware to use for all requests
+apiRouter.use(function(req, res, next) {
+    //do loggin
+    console.log('Our API is in use');
+
+    next();
+});
+
+//on routes that ends in /users
+//------------------------------------------------------------------------------
+apiRouter.route('/users')
+
+        //create a user(POST)
+        .post(function(req, res) {
+            
+            //create a new instance of the User models
+            var user = new User();
+
+            //set the user information (comes from the request)
+            user.name = req.body.name;
+            user.username = req.body.username;
+            user.password = req.body.password;
+
+            //save the user and check for errors
+            user.save(function(err){
+                if (err) {
+                    //dublicate entry
+                    if (err.code == 11000)
+                    return res.json({ sucess: false, message: 'A user with that usernamea already exists'});
+                    else
+                        return res.send(err);
+                }
+                        res.json({message: 'User created'});
+            });
+        })
+
+        //get all the users (GET)
+        .get(function(req, res) {
+            User.find(function(err, users) {
+                if (err) res.send(err);
+                   
+                    //return the users
+                    res.json(users);
+            });
+        });
+
+
+
 //test route to see if everything is working
 //accessed at GET http://localhost:8080/apiRouter
 apiRouter.get('/', function(req, res) {
