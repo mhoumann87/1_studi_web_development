a6ef25f03df5cefea234283c29741404d4e916a3
Michael Houmann <michael@michael-h.dk>
2016-11-16 19:03:45 +0100

    final version

--- ros_torv_assignment/public/js/app.routes.js
+++ ros_torv_assignment/public/js/app.routes.js
@@ -7,23 +7,23 @@ angular.module('routerRoutes', ['ngRoute'])
 
         // route for the home page
         .when('/', {
-            templateUrl : 'views/pages/home.html',
+            templateUrl : 'views/pages/forside.html',
             controller  : 'homeController',
-            controllerAs: 'home'
+            controllerAs: 'forside'
         })
 
         // route for the about page
-        .when('/about', {
-            templateUrl : 'views/pages/about.html',
+        .when('/ugens_tilbud', {
+            templateUrl : 'views/pages/ugens_tilbud.html',
             controller  : 'aboutController',
-            controllerAs: 'about'
+            controllerAs: 'ugens_tilbud'
         })
 
         // route for the contact page
-        .when('/contact', {
-            templateUrl : 'views/pages/contact.html',
+        .when('/butik', {
+            templateUrl : 'views/pages/butik.html',
             controller  : 'contactController',
-            controllerAs: 'contact'
+            controllerAs: 'butik'
         });
 
     $locationProvider.html5Mode(true);
